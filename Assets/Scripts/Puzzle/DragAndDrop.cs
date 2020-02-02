cc75325ee5dbc176372bdf8ee251ddbe30b47534
CharlieBaena <carlos_baena_ariza@hotmail.com>
2020-02-02 02:27:13 +0100

    Ajustando la textura del fondo

--- Assets/Scripts/Puzzle/DragAndDrop.cs
+++ Assets/Scripts/Puzzle/DragAndDrop.cs
@@ -52,7 +52,7 @@ public class DragAndDrop : MonoBehaviour
     }
 
     private void OnMouseUp(){
-
+        //posInicial = transform.position;
         if (puedoMover)
         {
             transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0);
